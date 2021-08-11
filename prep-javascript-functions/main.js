720008bb1d0747eec957602719d4f15286770c71
Michael Moncera <michaelmoncera@gmail.com>
2021-08-10 21:16:57 -0700

    Implemented subtractTwoNumbers function.

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -32,3 +32,10 @@ function multiplyAndDivideBy5(num1, num2) {
 
 var multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
 console.log('multiplyAndDivideBy5 Exercise:', multiplyAndDivideBy5Result);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+var subtractTwoNumbersResult = subtractTwoNumbers(22, 7);
+console.log('subtractTwoNumbers Exercise:', subtractTwoNumbersResult);

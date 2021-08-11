cfface975e232eabb715ca61e0275e2be6d54c87
Michael Moncera <michaelmoncera@gmail.com>
2021-08-10 20:53:27 -0700

    Implemented convertHoursToMinutes function.

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -4,3 +4,13 @@ function addTwoNumbers(num1, num2) {
 
 var addTwoNumbersResult = addTwoNumbers(4, 4);
 console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log(
+  'convertHoursToMinutesResult Exercise:',
+  convertHoursToMinutesResult
+);
